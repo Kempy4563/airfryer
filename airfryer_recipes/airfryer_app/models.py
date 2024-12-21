5f72ee2649d4990f1507e0b706cb84e2af69d88c
leekempson <leekempson@hotmail.com>
2024-12-21 12:37:40 +0100

    users and recipe models associated with profile

--- airfryer_recipes/airfryer_app/models.py
+++ airfryer_recipes/airfryer_app/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from users.models import Profile
+from django.contrib.auth.models import User
+
+
 
 
 class Recipe(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=1)
     name = models.CharField(max_length=100)
     description = models.TextField()
     time = models.IntegerField(null=True)
--- airfryer_recipes/airfryer_recipes/urls.py
+++ airfryer_recipes/airfryer_recipes/urls.py
@@ -7,5 +7,6 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("airfryer_app.urls")),
+    path("users/", include("users.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
