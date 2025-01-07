5be4d6ea883a02a149d2bbbf3d89268a9cfd0b13
leekempson <leekempson@hotmail.com>
2025-01-07 16:16:06 +0100

    added media folder at root dir with images

--- airfryer_recipes/airfryer_recipes/settings.py
+++ airfryer_recipes/airfryer_recipes/settings.py
@@ -126,10 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'airfryer_app', 'static_files')
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
