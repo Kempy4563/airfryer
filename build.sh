cc1f223566997483be2d88451fbbb6c2eb08f858
leekempson <leekempson@hotmail.com>
2025-01-07 15:51:54 +0100

    updates requirements file

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+python airfryer_recipes/manage.py collectstatic --no-input
+python airfryer_recipes/manage.py migrate
