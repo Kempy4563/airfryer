44dd9fc9a737201570d5b819bafd81a778de8118
leekempson <leekempson@hotmail.com>
2025-01-08 12:02:58 +0100

    added media setting changes after allocating disk on render

--- airfryer_recipes/airfryer_recipes/settings.py
+++ airfryer_recipes/airfryer_recipes/settings.py
@@ -130,7 +130,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'airfryer_recipes', 'static_files')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/data/media'
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
