f6042051cf69a9fd5864e1e11748424eeed8cc04
leekempson <leekempson@hotmail.com>
2025-01-09 16:07:11 +0100

    added re_path to root urls

--- airfryer_recipes/airfryer_recipes/urls.py
+++ airfryer_recipes/airfryer_recipes/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.conf.urls.static import static, serve
 from django.conf import settings
 
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("airfryer_app.urls")),
     path("users/", include("users.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
