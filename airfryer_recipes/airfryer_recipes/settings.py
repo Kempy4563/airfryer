2c319f58e4738947a71b70d9fdc9ca25457da89f
leekempson <leekempson@hotmail.com>
2025-01-09 12:15:25 +0100

    test with hard coded directory for media files in html

--- airfryer_recipes/airfryer_recipes/settings.py
+++ airfryer_recipes/airfryer_recipes/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
             ],
         },
     },
@@ -128,7 +129,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/data/media'
+MEDIA_ROOT = '/var/data/media/'
 
 
 # Default primary key field type
