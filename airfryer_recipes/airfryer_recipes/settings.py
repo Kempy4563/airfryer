e9c62bf010d058bc54121a83afaa35256337a884
leekempson <leekempson@hotmail.com>
2025-01-09 12:24:45 +0100

    test with hard coded directory for media files in html

--- airfryer_recipes/airfryer_recipes/settings.py
+++ airfryer_recipes/airfryer_recipes/settings.py
@@ -129,7 +129,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/data/media/'
+MEDIA_ROOT = '/var/data/media'
 
 
 # Default primary key field type
