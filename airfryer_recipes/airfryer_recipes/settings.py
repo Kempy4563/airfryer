b861464fa1fdba5e6388a8ca77cffb206644dd3f
leekempson <leekempson@hotmail.com>
2025-01-10 17:49:46 +0100

    stlye setting to make images same size

--- airfryer_recipes/airfryer_recipes/settings.py
+++ airfryer_recipes/airfryer_recipes/settings.py
@@ -132,9 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# uncomment these in prod
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #MEDIA_URL = '/media/'
 #MEDIA_ROOT = '/var/data'
